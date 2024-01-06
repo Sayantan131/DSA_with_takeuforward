67739505c1b1edc01b8f90b0ff532716f1c3f07e
Sayantan <maitysayantan111@gmail.com>
2024-01-06 15:47:04 +0530

    some doubt occurs

--- function2.cpp
+++ function2.cpp
@@ -18,16 +18,34 @@ using namespace std;
 // }
 
 //pass by  reference
-void dosomething(string &s){
-s[0] = 'a';
-cout<<s<<endl;
+// void dosomething(string &s){
+// s[0] = 'a';
+// cout<<s<<endl;
+// }
+
+// int main(){
+    
+//     string s;
+//     cin>>s;
+//     dosomething(s);
+//     cout<<s<<endl;
+//  return 0;   
+// }
+
+void dosomething(int arr[], int n){
+
+   arr[0] +=100;
+   cout<<"Value inside the function: "<<arr[0]<<endl;
+
 }
 
 int main(){
-    
-    string s;
-    cin>>s;
-    dosomething(s);
-    cout<<s<<endl;
- return 0;   
+    int n = 5;
+    int arr[n];
+    for(int i=0;i<=n-1;i++){
+        cin>>arr[i];
+    }
+    dosomething(arr,n);
+    cout<<"Value inside the main: "<<arr[0]<<endl;
+    return 0;
 }
