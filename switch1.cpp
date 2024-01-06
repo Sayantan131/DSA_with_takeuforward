e8b21fb5257f603b25dc3b07ebb47299cc576d8e
Sayantan <maitysayantan111@gmail.com>
2024-01-06 08:57:24 +0530

    switch done

---
+++ switch1.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+Take the day no and print the corresponding day
+for 1 print Monday
+for 2 print Tuesday abd so on for 7 print sunday.
+*/
+
+int main(){
+    int day;
+    cin>>day;
+    switch(day){
+        case 1:
+        cout<<"Monday";
+        break;
+        case 2:
+        cout<<"Tuesday";
+        break;
+        case 3:
+        cout<<"wednesday";
+        break;
+        case 4:
+        cout<<"Thursday";
+        break;
+        case 5:
+        cout<<"Friday";
+        break;
+        case 6:
+        cout<<"Saturday";
+        break;
+        case 7:
+        cout<<"Sunday";
+        break;
+        default:
+        cout<<"Invalid check!";
+    }
+}
