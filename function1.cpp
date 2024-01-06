e7a7ca2cb553ad4e362c30561b8519bb9960f88a
Sayantan <maitysayantan111@gmail.com>
2024-01-06 09:42:40 +0530

    funtion

---
+++ function1.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    //Functions are set of code which performs something for you
+    //Functions are used to modularise code
+    //Functions are used to increase readibility 
+    //Functions are used to use same code multiple times
+    //void -> which does not return anything
+    //return 
+    //parameterized 
+    //non parameterized
+
+    void printName(string name)
+    {
+        cout<<"Hey "<<name<<endl;
+    }
+    int main(){
+        string name;
+        cin>>name;
+        printName(name);
+        string name2;
+        cin>>name2;
+        printName(name2);
+        return 0;
+    }
