ff30f8207690c6d2bea2d29b4be06ce6a63fe2de
Sayantan <maitysayantan111@gmail.com>
2024-01-10 11:49:37 +0530

    learning

---
+++ logic_building/pattern3.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+pattern3(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+    int n;
+    cin>>n;
+    pattern3(n);
+    }
+   
+}
