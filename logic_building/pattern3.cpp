eaa8955d4ad87f6c2576ee1b1ba501865fa25a8f
Sayantan <maitysayantan111@gmail.com>
2024-01-11 20:32:57 +0530

    pattern8 done

--- logic_building/pattern3.cpp
+++ logic_building/pattern3.cpp
@@ -41,7 +41,46 @@ pattern6(int n){
 
 
 pattern7(int n){
-    
+    for(int i=0;i<n;i++){
+        //space
+    for(int j=0;j<n-i-1;j++){
+        cout<<" ";
+    }
+        //star
+    for(int j=0;j<2*i+1;j++){
+        cout<<"*";
+    }
+
+        //space
+        for(int j=0;j<n-i-1;j++){
+        cout<<" ";
+    }
+    cout<<"\n";
+    }
+}
+
+
+pattern8(int n){
+
+    for(int i=n;i>0;i--){
+//space
+// cout<<"sayantan";
+for(int j=0;j<n*1-i;j++){
+    // cout<<"s";
+    cout<<" ";
+}
+//star
+for(int j=0;j<2*i-1;j++){
+    cout<<"*";
+}
+//space
+for(int j=0;j<n*1-i;j++){
+    // cout<<"s";
+    cout<<" ";
+}
+cout<<"\n";
+    }
+
 }
 
 
@@ -52,7 +91,7 @@ int main(){
     for(int i=0;i<t;i++){
     int n;
     cin>>n;
-    pattern7(n);
+    pattern8(n);
     }
    
 }
