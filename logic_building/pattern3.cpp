9b711387f674d26f3c68f9bff78aca5dcec0eb32
Sayantan <maitysayantan111@gmail.com>
2024-01-23 08:56:26 +0530

    pattern 17

--- logic_building/pattern3.cpp
+++ logic_building/pattern3.cpp
@@ -182,8 +182,25 @@ for(int i=0;i<n;i++){
 
 
 
-pattern18(int n){
-    
+pattern17(int n){
+
+
+    for(int i=0;i<n;i++){
+        for(int j=1;j<=n-i-1;j++){
+            cout<<" ";
+        }
+        char ch = 'A';
+        int breakpoint = (2*i+1)/2;
+        for (int j=1;j<=2*i+1;j++){
+            cout<<ch;
+            if(j<=breakpoint)ch++;
+            else ch--;
+        }
+        for(int j=1;j<=n-i-1;j++){
+            cout<<" ";
+        }
+        cout<<"\n";
+    }
 }
 
 
