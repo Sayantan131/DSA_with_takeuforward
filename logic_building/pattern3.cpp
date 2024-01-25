22b2c6af93de47335c87e7bb3bc20f4be9aa700c
Sayantan <maitysayantan111@gmail.com>
2024-01-25 23:10:38 +0530

    pattern 21

--- logic_building/pattern3.cpp
+++ logic_building/pattern3.cpp
@@ -281,7 +281,20 @@ pattern20(int n){
 }
 
 
-
+pattern21(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==0||i==n-1||j==0||j==n-1){
+                cout<<"*";
+            }
+             else
+                    cout<<" ";
+                
+            
+        }
+        cout<<"\n";
+    }
+}
 
 int main(){
     int t;
@@ -289,7 +302,7 @@ int main(){
     for(int i=0;i<t;i++){
     int n;
     cin>>n;
-   pattern20(n);
+   pattern21(n);
     }
    
 }
