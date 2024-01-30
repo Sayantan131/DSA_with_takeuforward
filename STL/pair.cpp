99628f9bd4c5a10d2f732a00876c1dbcf11dd4d1
Sayantan <maitysayantan111@gmail.com>
2024-01-30 13:14:06 +0530

    STL

---
+++ STL/pair.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void explainpair(){
+    pair<int,int>p={1,3};
+    cout<<p.first<<" "<<p.second<<endl;
+    pair<int,pair<int,int>p={1,{3,4}};
+    cout<<p.first<<" "<<p.second.first<<" "<<p.second.second<<endl;
+}
+
+explainpair();
