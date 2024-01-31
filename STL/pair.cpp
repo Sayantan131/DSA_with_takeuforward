1030b6aec9caceca4b2faf9b8b2854bb206a5128
Sayantan <maitysayantan111@gmail.com>
2024-01-31 12:29:28 +0530

    just push

--- STL/pair.cpp
+++ STL/pair.cpp
@@ -1,16 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int factorial(int n){
-    if(n == 0 || n==1){
-        return 1;
-    }else{
-        return n*factorial(n-1);
+// int factorial(int n){
+//     if(n == 0 || n==1){
+//         return 1;
+//     }else{
+//         return n*factorial(n-1);
+//     }
+// }
+
+// int main(){
+//     int num;
+//     cin>>num;
+//     cout<<"fac: "<<num<<"  " <<factorial(num);
+// }
+
+countDigit(int n){
+    int cnt = 0;
+    while(n>0){
+       int  lastdigit = n%10;
+        cnt = cnt+1;
+        n = n/10;
     }
+    return cnt;
 }
 
+
+
 int main(){
-    int num;
-    cin>>num;
-    cout<<"fac: "<<num<<"  " <<factorial(num);
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+    int n;
+    cin>>n;
+   problem1(n);
+    }
+   
 }
