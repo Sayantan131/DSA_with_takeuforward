cc0a17191e39475a3f09e62a8dd5681ac27b9de2
Sayantan <maitysayantan111@gmail.com>
2024-02-13 23:42:05 +0530

    factorial

--- Recursion/tempCodeRunnerFile.cpp
+++ Recursion/tempCodeRunnerFile.cpp
@@ -1,12 +1,3 @@
-int main(){
-//     #ifndef ONLINE_JUDGE
-//     freopen("input.txt", "r", stdin);
-//     freopen("output.txt","w",stdout);
-//     #endif
-//       int n;
-//     cin>>n;
-//     // cout<<n;
-//    cout<<countDigit(n);
-  
-   
-// }
+#include<bits/stdc++.h>
+using namespace std;
+
---
+++ Recursion/ten copy.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+mul(int n){
+    if(n<1){
+        return 0;
+    }
+    return n*mul(n-1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+   
+    int n;
+    cin>>n;
+    cout<< mul(n);
+    return 0;
+}
