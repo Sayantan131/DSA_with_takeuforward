8738a55fbc9f80e6c10ec69de26412e59c046594
Sayantan <maitysayantan111@gmail.com>
2024-02-19 02:37:26 +0530

    code is not running accurately

--- Recursion/eleven.cpp
+++ Recursion/eleven.cpp
@@ -1,3 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void f(int i, int arr[], int n){
+    if(i>=n/2){
+        return;
+    }
+    swap(arr[i],arr[n-i-1]);
+    cout<<"Say";
+    f(i+1, arr, n);
+}
+int main(){
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt","w",stdout);
+    // #endif
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    f(0,arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }   
+    return 0;
+}
