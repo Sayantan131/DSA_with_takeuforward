d389a1c45428ba0c1fe1047ff092952f53021dfb
Sayantan <maitysayantan111@gmail.com>
2024-02-23 19:41:02 +0530

    multiple recursion

---
+++ Recursion/fourteen.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int f(int n){
+    if(n <= 1) return n;
+    int start = f(n-1);
+    int last = f(n-2);
+    return last+start;
+}
+
+int main(){
+    cout<<f(4);
+    return 0;
+}
